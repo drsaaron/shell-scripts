54c865c13019622a2d3b6723302745f05403b9ff
aar1069 <scottaaron@northwesternmutual.com>
2021-06-09 14:24:46 -0500

    expand dockerCleanup to more cleanly handle not having images to cleanup

--- dockerCleanup.sh
+++ dockerCleanup.sh
@@ -1,3 +1,14 @@
 #! /bin/sh
 
-docker rmi -f $(docker images -qf "dangling=true")
+tmpFile=/tmp/docker-dangling
+
+rm -f $tmpFile
+docker images -qf "dangling=true" > $tmpFile
+
+if [ -s $tmpFile ]
+then
+    docker rmi -f $(cat $tmpFile)
+else
+    echo "no images to purge." 1>&2
+fi
+
