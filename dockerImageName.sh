8ad0a0d8470c712717b8f91df8b22871e8bb9016
Scott Aaron <dr_saaron@yahoo.com>
2021-11-24 08:42:40 -0600

    support buildImage.sh files as well to find the image name

--- dockerImageName.sh
+++ dockerImageName.sh
@@ -1,14 +1,19 @@
 #! /bin/sh
 
+prefix=drsaaron
+
 if [ -f pom.xml ]
 then
-    imageName=$(getPomAttribute.sh artifactId | tr '[:upper:]' '[:lower:]')
+    imageName=$prefix/$(getPomAttribute.sh artifactId | tr '[:upper:]' '[:lower:]')
 elif [ -f package.json ]
 then
-    imageName=$(getPackageJsonAttribute.sh name | tr '[:upper:]' '[:lower:]')
+    imageName=$prefix/$(getPackageJsonAttribute.sh name | tr '[:upper:]' '[:lower:]')
+elif [ -f buildImage.sh ]
+then
+    imageName=$(grep ^imageName= buildImage.sh | awk -F = '{ print $2 }')
 else
     echo "unable to determine image name" 1>&2
     exit 1
 fi
 
-echo drsaaron/$imageName
+echo $imageName
