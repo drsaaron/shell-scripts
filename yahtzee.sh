5b2956be9ad4a5df1d239c99ad049e7886248d64
Scott Aaron <dr_saaron@yahoo.com>
2022-04-16 08:29:48 -0500

    add yahtzee.sh

---
+++ yahtzee.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+# simple script to start or stop yahtzee container depending on its current
+# run state
+container=yahtzee
+
+docker ps | grep -q $container && action=stop || action=start
+echo "$action-ing $container"
+docker $action $container
