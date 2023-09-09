dc4605acf2d26ffb445f0307f0bec2bbd8b3e6f8
Scott Aaron <dr_saaron@yahoo.com>
2023-09-09 09:08:29 -0500

    add BlazrUserManagement projects to qotdUpdate

--- qotdUpdate.sh
+++ qotdUpdate.sh
@@ -3,4 +3,31 @@
 cd ~/NetBeansProjects/BlazarJavaBase
 updateImage.sh
 
-bulkMavenUpdate.sh BlazarCryptoFile BlazarFacebookClient BlazarJobManager BlazarMailer BlazarMailer-springImpl DateServices TelegramClient QuoteOfTheDay-data QuoteOfTheDay-data-jpaImpl QuoteOfTheDay-process QuoteOfTheDayServices QuoteOfTheDayJob
+dbstarted=false
+mysql=mysql1
+startDB() {
+    if docker ps |grep -q $mysql
+    then
+	echo "$mysql is already running"
+    else
+	echo "starting $mysql"
+	docker start $mysql
+	dbstarted=true
+    fi
+}
+
+stopDB() {
+    if [ "$dbstarted" = "true" ]
+    then
+	echo "stopping $mysql"
+	docker stop $mysql
+    fi
+}
+
+# start mysql, which is needed to build BlazarUserManagement.  
+startDB
+
+# set a trap so that when this process exits, if the DB was started by it the DB will also be stopped
+trap "stopDB; exit 0" INT EXIT
+
+bulkMavenUpdate.sh BlazarUserManagement-tokenUtil BlazarUserManagement-serverUtil BlazarUserManagement BlazarCryptoFile BlazarFacebookClient BlazarJobManager BlazarMailer BlazarMailer-springImpl DateServices TelegramClient QuoteOfTheDay-data QuoteOfTheDay-data-jpaImpl QuoteOfTheDay-process QuoteOfTheDayServices QuoteOfTheDayJob
