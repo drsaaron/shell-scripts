9627e781eccc21402cc4aa2d6b9fce9a84cfa9e7
Scott Aaron <dr_saaron@yahoo.com>
2024-03-21 07:24:57 -0500

    fix update playbook for md5 and pip3-update for output

--- pip3-update.sh
+++ pip3-update.sh
@@ -1,7 +1,19 @@
 #! /bin/sh
 
+workFile=/tmp/pip3-update.txt
+
 pip3 list --outdated |
     sed -e '1,2d' |
     grep -v '^\-e' |
-    awk '{print $1}' |
-    xargs -I % sh -c "echo \"updating %\"; pip3 install -U %"
+    awk '{print $1}' > $workFile
+
+if [ -s $workFile ]
+then
+    xargs -I % sh -c "echo \"updating %\"; pip3 install -U %" < $workFile
+
+    echo "update complete, updated"
+    cat $workFile
+else
+    echo "no packages to update"
+fi
+
