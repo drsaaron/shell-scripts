19635bd738779e8f5fa59e78ceddf83ef0341cd6
Scott Aaron <dr_saaron@yahoo.com>
2026-05-15 16:01:18 -0500

    add deploy docker

---
+++ deployDocker.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+while getopts :i: OPTION
+do
+    case $OPTION in
+	i)
+	    imageName=$OPTARG
+	    ;;
+	*)
+	    echo "invalid option $OPTARG" 1>&2
+	    exit 1
+    esac
+done
+
+if [ -z "$imageName" ]
+then
+    if [ -f pom.xml ]
+    then
+	version=$(getPomAttribute.sh version | sed -e 's/-[A-Z]*$//')
+    elif [ -f package.json ]
+    then
+	version=$(getPackageJsonAttribute.sh version)
+    else
+	echo "unable to determine version" 1>&2
+	exit 1
+    fi
+
+    imageName=$(dockerImageName.sh):$version
+fi
+
+echo "copying $imageName to $LAPTOP_IP"
+docker save $imageName | gzip | ssh $(whoami)@$LAPTOP_IP "gunzip | docker load"
